8f713bc1213c215add9135ce5b835285005a0d3f
thafernand <thays.fernandes.senai404@gmail.com>
2024-08-16 09:35:39 -0300

    Add files via upload

---
+++ exercicio_return.php
@@ -0,0 +1,20 @@
+<?php
+
+function tabuada ($tabuada) {
+    $cont = 1;
+
+    while ($cont <= 10){
+        $resultado = $tabuada * $cont;
+        echo ($tabuada . "x" . $cont ." = ". $)
+        $cont + +;
+    }
+}
+
+$tabela = '<center><table border = "1"; >
+$tabela .= '<tr>';
+$tabela .= '<td> tabuada (10)</td>';
+$tabela .= '</tr>' ;
+$tabela .= '</table></center>';
+echo $tabela;
+
+?>
---
+++ funcao_return.php
@@ -0,0 +1,10 @@
+<?php
+// Declaração de uma função que retorna um valor
+function soma ($a, $b) {
+    return $a + $b;
+}
+
+// Utilizando o valor retornando pela função
+$resultado = soma(5, 3); // $resultado será 8
+echo $resultado; // Saída: 8
+?>
